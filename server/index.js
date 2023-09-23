539ef418ec8c230f6a8b72c0222a9da8ceb74bb9
zahrakht <zahra.kh.t@gmail.com>
2023-09-23 15:39:45 +0330

    add socket.io

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const { connectDB } = require("./db/connect.js");
 const userRouter = require("./routes/user.router.js");
 const messageRouter = require("./routes/message.router.js");
-
+const socket = require("socket.io");
 const app = express();
 require("dotenv").config();
 
@@ -16,12 +16,36 @@ app.use("/api/message", messageRouter);
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URL);
-        app.listen(process.env.PORT, () => {
+        const server = app.listen(process.env.PORT, () => {
             console.log(`server is running on port ${process.env.PORT}`);
         });
+        createSocket(server);
     } catch (error) {
         console.log(error);
     }
+};
+
+const createSocket = (server) => {
+    const io = socket(server, {
+        cors: {
+            origin: "http://localhost:3000",
+            credentials: true,
+        },
+    });
+    global.onlineUsers = new Map();
+    io.on("connection", (socket) => {
+        global.chatSocket = socket;
+        socket.on("add-user", (userId) => {
+            onlineUsers.set(userId, socket.id);
+        });
+        socket.on("send-msg", (data) => {
+            const sendUserSocket = onlineUsers.get(data.to);
+
+            if (sendUserSocket) {
+                socket.to(sendUserSocket).emit("msg-receive", data.message);
+            }
+        });
+    });
 };
 
 start();
